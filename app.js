8802f91348dfc36189ded6e18d3240b1ddfa8855
Facundo Becerra <facubecerra@live.com.ar>
2026-05-07 21:49:15 -0300

    correccion css y actualizacion javascript

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const obtenerPersonajes = async ()=>{
     try{
-        const respuesta = await fetch('https://rickandmortyapi.com/api/character')
+        const respuesta = await fetch('./personajes.json')
         const datos = await respuesta.json()
         return datos.results
     } catch(error){
-        console.error('Error al obtener personajes:', error)
+       Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'No se pudieron cargar los personajes.'
+})
     }
 }
 
@@ -13,7 +17,7 @@ obtenerPersonajes()
 const devuelvePersonajes = (personajes)=>{ 
     const section = document.getElementById('card')
     const cards = personajes.map(personaje => `
-        <div class="card">
+        <div class="card" data-id="${personaje.id}">
         <img src="${personaje.image}" alt="${personaje.name}">
         <h2>${personaje.name}</h2>
         <p>${personaje.status}</p>
@@ -28,17 +32,70 @@ let personajes = []
 const init = async()=>{
     personajes = await obtenerPersonajes()
     devuelvePersonajes(personajes)
+    mostrarContador(personajes)
+
     const filtrar = document.getElementById('filtro-estado')
+    const filtroAlfabetico = document.getElementById('ordenar')
+    const section = document.getElementById('card')
+
+      filtroAlfabetico.addEventListener('change',(event)=>{
+        const orden = event.target.value
+        const ordenados = [...personajes].sort((a,b)=>orden==='az'? a.name.localeCompare(b.name):b.name.localeCompare(a.name))
+        devuelvePersonajes(ordenados)
+        
+    })
     filtrar.addEventListener('change',(event)=>{
         filtroAplicado = event.target.value
     const filtrado = personajes.filter(personaje => personaje.status === filtroAplicado)
 
+
     if(filtroAplicado === 'todos'){
         devuelvePersonajes(personajes)
+            mostrarContador(personajes)
+
+        Toastify({
+        text: `Mostrando ${personajes.length} personajes`,
+        duration: 3000,
+        gravity: "top",
+        position: "right",
+        style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+        color: "white",
+    }
+}).showToast()
+
     }else{
         devuelvePersonajes(filtrado)
+            mostrarContador(filtrado)
+
+        Toastify({
+        text: `Mostrando ${filtrado.length} personajes`,
+        duration: 3000,
+        gravity: "top",
+        position: "right",
+        style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+        color: "white",
+    }
+}).showToast()
+
     }
     })
+
+    section.addEventListener('click', (event) =>{
+        const card = event.target.closest('.card')
+        if(card) {
+        const id = parseInt(card.dataset.id)
+        const personajeEncontrado = personajes.find(p => p.id === id)
+            Swal.fire({
+                title: personajeEncontrado.name,
+                text: `Estado: ${personajeEncontrado.status} -- Especie: ${personajeEncontrado.species}`,
+                imageUrl: personajeEncontrado.image,
+                imageWidth: 300,
+            })
+        }
+    })
+
     const buscar = document.getElementById('buscador')
     buscar.addEventListener('input',(event)=>{
         filtroPorInput = event.target.value
@@ -49,10 +106,18 @@ const init = async()=>{
 }
 init()
 
-const contarPorEstado = (personajes) =>{
-    personajes.reduce((acum,personaje)=>{
+const contarPorEstado = (personajes) => {
+     return personajes.reduce((acum, personaje) => {
         acum[personaje.status] = (acum[personaje.status] || 0) + 1
         return acum
-    },{})
+    }, {})
 }
 
+const mostrarContador = (personajes) => {
+    const conteo = contarPorEstado(personajes)
+    const div = document.getElementById('contador-estados')
+    div.innerHTML = `Alive: ${conteo.Alive || 0} | Dead: ${conteo.Dead || 0} | Unknown: ${conteo.unknown || 0}`
+}
+
+//falta storage, falta librerias,
+//modularizar init tambien filtro y buscador
